b327166094b5f2b8705d389ad0cadac5201b6ad6
Cloxton <66524628+cloxton2003@users.noreply.github.com>
2020-09-02 23:41:36 -0600

    Update script2.js

--- script2.js
+++ script2.js
@@ -1,20 +1,37 @@
 const button = document.getElementById("enter");
 const input = document.getElementById("userInput");
-const list = document.querySelector("ul");
+const list = document.getElementsByTagName("ul")[0];
 
-buttonClicked = () => {
+button.addEventListener("click", buttonClicked = () => {
     let x = input.value.length;
     if (x > 0) {
         const li = document.createElement("li");
         li.innerHTML = input.value;
         list.appendChild(li);
         input.value = "";
+        createASickBtn(li);
     }
+});
+
+createASickBtn = (li) => {
+	    let btn = document.createElement("button");
+	    btn.classList.add("myButton");
+        li.appendChild(btn);
+        let btnText = document.createTextNode("Delete");
+        btn.appendChild(btnText);
+        btn.onclick = delItem;
 }
 
-list.onclick = elementClicked = (param1) => {
-    const listItem = param1.target;
-    listItem.classList.toggle("done");
+delItem = (y) => {
+	y.target.parentNode.remove();
+}
+
+list.onclick = elementClicked = (x) => {
+   	let y = x.target;
+    y.classList.toggle("done");
 } 
 
-button.addEventListener("click", buttonClicked);
+delBtn = () => {
+	alert("damn");
+}
+
